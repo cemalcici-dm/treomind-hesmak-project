0b377c7d5c8dd5b528723afd0d70cb8b063e23b2
cemalcici <iecemalcici@gmail.com>
2025-04-10 21:59:29 +0300

    app modülü eski haline getirildi.

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-import hesmak
+from hesapla import hesap_makinesi
 
-print(hesmak.__version__)
-
-# from hesapla import hesap_makinesi
-
-# cikis_yapildi_mi = False
-# while not cikis_yapildi_mi:
-#     print('[1]: Toplama', '[2]: Çıkarma', '[3]: Çarpma', '[4]: Bölme', sep='\n')
-#     secenek = input("Bir seçenek giriniz (Çıkış için 0'a basınız): ")
-#     if secenek == '0':
-#         print('Program başarılı bir şekilde sonlandırılmıştır.')
-#         cikis_yapildi_mi = True
-#     elif secenek == '1' or secenek == '2' or secenek == '3' or secenek == '4':
-#         birinci_sayi = float(input('Birinci sayıyı giriniz: '))
-#         ikinci_sayi = float(input('İkinci sayıyı giriniz: '))
-#         if ikinci_sayi == 0.0 and secenek == '4':
-#             print("Hatalı bölme işlemi. Baştan başlıyor...")
-#             continue
-#         print(f"Sonuç: {hesap_makinesi(birinci_sayi, ikinci_sayi, int(secenek))}")
-#         print("Hesaplama başarılı. Yeniden başlatılıyor.")
-#     else:
-#         print('Hatalı seçenek girdiniz!')
+cikis_yapildi_mi = False
+while not cikis_yapildi_mi:
+    print('[1]: Toplama', '[2]: Çıkarma', '[3]: Çarpma', '[4]: Bölme', sep='\n')
+    secenek = input("Bir seçenek giriniz (Çıkış için 0'a basınız): ")
+    if secenek == '0':
+        print('Program başarılı bir şekilde sonlandırılmıştır.')
+        cikis_yapildi_mi = True
+    elif secenek == '1' or secenek == '2' or secenek == '3' or secenek == '4':
+        birinci_sayi = float(input('Birinci sayıyı giriniz: '))
+        ikinci_sayi = float(input('İkinci sayıyı giriniz: '))
+        if ikinci_sayi == 0.0 and secenek == '4':
+            print("Hatalı bölme işlemi. Baştan başlıyor...")
+            continue
+        print(f"Sonuç: {hesap_makinesi(birinci_sayi, ikinci_sayi, int(secenek))}")
+        print("Hesaplama başarılı. Yeniden başlatılıyor.")
+    else:
+        print('Hatalı seçenek girdiniz!')
