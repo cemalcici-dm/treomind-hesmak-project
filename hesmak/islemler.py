542cb0099e7dd33900050a2ad786a02d11b58cc2
cemalcici <iecemalcici@gmail.com>
2025-04-10 23:23:20 +0300

    islemler modülüne log eklendi.

--- hesmak/islemler.py
+++ hesmak/islemler.py
@@ -2,16 +2,20 @@
 
 
 def topla(sayi_1, sayi_2):
+    print("Toplama işlemi yapıldı.")
     return sayi_1 + sayi_2
 
 
 def cikar(sayi_1, sayi_2):
+    print("Çıkarma işlemi yapıldı.")
     return sayi_1 - sayi_2
 
 
 def carp(sayi_1, sayi_2):
+    print("Çarpma işlemi yapıldı.")
     return sayi_1 * sayi_2
 
 
 def bol(sayi_1, sayi_2):
+    print("Bölme işlemi yapıldı.")
     return sayi_1 / sayi_2
